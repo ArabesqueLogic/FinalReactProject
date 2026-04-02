77ee6af35514c9df7f211b64b40c84f51c0c95c2
ArabesqueLogic <bozomechanics@gmail.com>
2026-04-02 15:53:47 -0400

    Modifications

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { ThemeProvider } from "./ThemeContext";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import HomePage from "./pages/HomePage";
